38857d555d2493edf0b0c46fa1ab39fab83b5939
galishai <gali.ishai@gmail.com>
2024-11-05 14:56:26 +0200

    tested on first game

--- random_forest.py
+++ random_forest.py
@@ -4,76 +4,72 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
+import csv
 
+#0 for classification, 1 for probabilities
+PROBABILITIES = 1
+
+
+TRAIN_LAST_ROW_CSV = 379
 file_path = 'output_v3_with_target_vf.csv'
+
+
+def seconds_to_time(seconds):
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return f"{minutes}:{remaining_seconds:02d}"
+
+
 dataset = pd.read_csv(file_path)
+modified_dataset = dataset.copy()
 
-original_data = dataset.copy()
-
-#data = dataset.drop(columns=['is_highlight'])
-#target = dataset['is_highlight']
-#print("Data type of 'games_played':", data['time_left_qtr'].dtype)
-
-train_data = dataset.iloc[378:]  # All rows except the last
-test_data = dataset.iloc[:378]   # Only the last row
-
-original_test_data = test_data.copy()
-
-
-#encoding categorical columns
+# encoding categorical columns
 label_encoders = {}
-for column in dataset.select_dtypes(include=['object']).columns:
+for column in modified_dataset.select_dtypes(include=['object']).columns:
     label_encoders[column] = LabelEncoder()
-    dataset[column] = label_encoders[column].fit_transform(dataset[column])
+    modified_dataset[column] = label_encoders[column].fit_transform(modified_dataset[column])
 
-train_data = dataset.iloc[378:]  # All rows except the last
-test_data = dataset.iloc[:378]   # Only the last row
+data = modified_dataset.drop(columns=['is_highlight'])
+target = modified_dataset['is_highlight']
+#print("Data type of 'games_played':", data['time_left_qtr'].dtype)
+X_train = data.iloc[TRAIN_LAST_ROW_CSV - 1:]  # All rows except the last
+Y_train = target.iloc[TRAIN_LAST_ROW_CSV - 1:]
+X_test = data.iloc[:TRAIN_LAST_ROW_CSV - 1]  # Only the last row
+Y_test = target.iloc[:TRAIN_LAST_ROW_CSV - 1]
 
-#splitting
-#X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3, random_state=42)
-
-X_train = train_data.drop(columns=['is_highlight'])
-y_train = train_data['is_highlight']
-X_test = test_data[X_train.columns]  # Ensures we have the same columns
+test_non_encode = dataset.iloc[:TRAIN_LAST_ROW_CSV - 1]
 
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
-rf.fit(X_train, y_train)
+rf.fit(X_train, Y_train)
 
-# Make predictions
-'''
-y_pred = rf.predict(X_test)
+if PROBABILITIES == 1:
+    y_test_proba = rf.predict_proba(X_test)[:, 1]  # Probability for class 1
+    test_non_encode['probability_class_1'] = y_test_proba
 
-# Evaluate
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-print("Classification Report:\n", classification_report(y_test, y_pred))
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+    sorted_probabilities = test_non_encode.sort_values(by='probability_class_1', ascending=False)
+    sorted_probabilities['time_left_qtr'] = sorted_probabilities['time_left_qtr'].apply(seconds_to_time)
+    sorted_probabilities.to_csv('output_test_sorted_probabilities.csv', index=False)
+    print(f"Play component text saved to output_test_sorted_probabilities.csv")
+    print("OK :)")
+else:
+    Y_pred = rf.predict(X_test)
 
-# probabilities of samples being 1
-y_proba = rf.predict_proba(X_test)
-# Get the probability of each sample being class 1
-y_proba_1 = y_proba[:, 1]  # This selects the second column, which corresponds to class 1
+    # Evaluate
+    accuracy = accuracy_score(Y_test, Y_pred)
+    print("Accuracy:", accuracy)
+    print("Classification Report:\n", classification_report(Y_test, Y_pred))
+    print("Confusion Matrix:\n", confusion_matrix(Y_test, Y_pred))
 
-probability_df = original_data.iloc[X_test.index].copy()  # Get non-encoded samples
-probability_df['probability_class_1'] = y_proba_1  # Add probabilities as a new column
-top_100 = probability_df.sort_values(by='probability_class_1', ascending=False).head(100)
+    test_non_encode['classified'] = Y_pred
+    test_non_encode['time_left_qtr'] = test_non_encode['time_left_qtr'].apply(seconds_to_time)
+    test_non_encode.to_csv('output_test_classified.csv', index=False)
+    print(f"Play component text saved to output_test_classified.csv")
+    print("OK :)")
 
-# Print the top 100 samples with the highest probability of being class 1
-print("Top 100 samples with the highest probability of being class 1:")
-print(top_100)
-'''
 
-y_test_proba = rf.predict_proba(X_test)[:, 1]  # Probability for class 1
 
-# Add the prediction to the original test data for interpretation
-original_test_data['probability_class_1'] = y_test_proba
 
-sorted_probabilities = original_test_data.sort_values(by='probability_class_1', ascending=False)
-# Display the probability for the last game
-print("Probability of plays being class 1:")
-with pd.option_context('display.max_rows', None):
-    print("Sorted probabilities for the last game:")
-    print(sorted_probabilities)
+
 
 
 
